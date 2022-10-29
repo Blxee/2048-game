a66739b4db8316e6e2223008787f44cedfa45c68
HexKnight <alaouiamine23@gmail.com>
2022-10-30 00:31:57 +0100

    created the Spot class

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useState } from 'React'
+
+class Spot {
+  constructor() {
+    this.number = 0
+  }
+
+  plus(that) {
+    return new Spot(this.number + that.number)
+  }
+
+  toString() {
+    if (this.number <= 0) return ' '
+    else return this.number.toString()
+  }
+}
 
 function App() {
+
+  useState()
+
   return (
     <>
       <h1>Hello, World!</h1>
