a4d420945777d1a61a205a5392f038a71d41f063
HexKnight <alaouiamine23@gmail.com>
2022-11-04 22:22:25 +0100

    added half gruvbox as cell colors

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,50 @@ import './App.css';
 class Cell {
   static #colors = new Map();
   static {
-    this.#colors.set(2, 7)
-  }
+    this.#colors.set(2 ** 1, {
+      primary: '#8ec07c',
+      secondary: '#689d6a',
+    });
+    this.#colors.set(2 ** 2, {
+      primary: '#d3869b',
+      secondary: '#b16286',
+    });
+    this.#colors.set(2 ** 3, {
+      primary: '#83a598',
+      secondary: '#458588',
+    });
+    this.#colors.set(2 ** 4, {
+      primary: '#fabd2f',
+      secondary: '#d79921',
+    });
+    this.#colors.set(2 ** 5, {
+      primary: '#b8bb26',
+      secondary: '#98971a',
+    });
+    this.#colors.set(2 ** 6, {
+      primary: '#fe8019',
+      secondary: '#d65d0e',
+    });
+    this.#colors.set(2 ** 7, {
+      primary: '#fb4934',
+      secondary: '#cc241d',
+    });
+  };
 
   constructor(number) {
-    this.number = number || 0;
+    this.number = number || 2;
   }
 
-  getColor() {
-    return '#ffff00';
+  getPrimaryColor() {
+    return Cell.#colors.get(this.number).primary;
+  }
+
+  getSecondaryColor() {
+    return Cell.#colors.get(this.number).secondary;
+  }
+
+  static #getRandColor() {
+    return '#000000';
   }
 
   isEmpty() {
@@ -33,7 +68,7 @@ function App() {
 
   const [grid, setGrid] = useState({
     size: 4,
-    cells: Array(4 ** 2).fill(new Cell(2048)),
+    cells: Array(4 ** 2).fill(new Cell(2)),
 
     get(x, y) {
       return this.cells[this.size * y + x];
@@ -53,7 +88,14 @@ function App() {
               {...(cell.isEmpty() ?
                 { className: 'empty' }
                 :
-                { className: 'filled', style: { backgroundColor: cell.getColor() } }
+                {
+                  className: 'filled',
+                  style: {
+                    backgroundColor: cell.getPrimaryColor(),
+                    border: `2px solid ${cell.getSecondaryColor()}`,
+                    boxShadow: `0 4px 0 ${cell.getSecondaryColor()}`
+                  }
+                }
               )}
             >
               {cell.toString()}
